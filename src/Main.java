b36f36f34c36d20ff1d1e0dd41d82c1afc3a481d
EduardovJS <eduardoviniciusjorge@gmail.com>
2025-03-17 14:33:55 -0300

    questao 4

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        Avaliacao estudante1 = new Avaliacao();
-        Avaliacao estudante2 = new Avaliacao(7, 4 , 10);
-        estudante1.nota1 = 7;
-        estudante1.nota2 = 4;
-        estudante1.nota3 = 10;
-
-        System.out.println("Media aritmetica do Estudante1  " + estudante1.MediaAritmetica());
-        System.out.println("Media aritmetica do Estudante2 " + estudante2.MediaAritmetica());
-        System.out.println("/");
-
-        Aluno a1 = new Aluno("Eduardo", "Engenharia de Software", new Avaliacao(9,10,3));
-        a1.info();
-        System.out.println("/");
-
-        Conta c1 = new Conta(111, "Eduardo", 2000, 500);
-        c1.info();
-        c1.sacar(200);
-        c1.deposito(500);
-        c1.info();
-
-        System.out.println("/");
-        Conta c2 = new Conta(222, "Vinicius", 2000, 500);
-        c2.transferir(c1, 2000);
-        c1.info();
-        System.out.println("//");
-        c2.info();
-
-        System.out.println("/");
-        Notas eduardo = new Notas();
-        eduardo.setNota1(9);
-        eduardo.setNota2(10);
-        eduardo.resultado();
-        eduardo.setNota1(2);
-        eduardo.resultado();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        Cofre c = new Cofre();
+        c.add(new Moeda("Euro", 0.5));
+        c.add(new Moeda("Euro", 1));
+        System.out.println("Total  de moedas: " + c.calcularTotal());
     }
 }
